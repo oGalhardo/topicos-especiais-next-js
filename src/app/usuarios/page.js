12b5406b17caf3037d8a628a224269ab7b5a7a4b
Junior <juniorwgalhardo@aluno.santoangelo.uri.br>
2025-05-06 21:26:10 -0300

    fistComiit

--- src/app/usuarios/page.js
+++ src/app/usuarios/page.js
@@ -4,23 +4,19 @@ import { useEffect, useState } from "react";
 
 export default function Home() {
   const [usuarios, setUsuarios] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
     buscarUsuarios();
   }, []);
 
   const buscarUsuarios = async () => {
-    setIsLoading(true);
     const response = await fetch("https://jsonplaceholder.typicode.com/users");
     const result = await response.json();
     setUsuarios(result);
-    setIsLoading(false);
   };
   return (
     <div>
-      <h1>Usuários Servidor</h1>
-      {isLoading && "carregando"}
+      <h1>Usuários</h1>
       <ul>
         {usuarios.map((item, index) => (
           <li key={index}>{item.name}</li>
