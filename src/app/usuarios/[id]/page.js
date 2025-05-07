3ffa039614901d2a4a2cbe886446db463caca49e
Junior <juniorwgalhardo@aluno.santoangelo.uri.br>
2025-05-06 21:44:09 -0300

    GetUser

--- src/app/usuarios/[id]/page.js
+++ src/app/usuarios/[id]/page.js
@@ -1,3 +1,19 @@
+"use client";
+import { act, use, useEffect, useState } from "react";
+
 export default function Page({ params }) {
-  return <>Usuarios {params.id}</>;
+  const [user, setUser] = useState(null);
+
+  const buscarUsuarios = async (actualParams) => {
+    const response = await fetch(
+      `https://jsonplaceholder.typicode.com/users/${actualParams}`
+    );
+    const result = await response.json();
+
+    setUser(result);
+  };
+  useEffect(() => {
+    buscarUsuarios(params.id);
+  }, [params]);
+  return <>Usuarios {user.name} </>;
 }
